1253e3109e1e60962a91811bc0775b25f5267298
wonseoke <wonseoke@gmail.com>
2020-06-13 21:04:51 -0400

    Write example data to CSV

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,7 +1,11 @@
 # app/robo_advisor.py
 
-import requests
+import csv
 import json
+import os
+
+import requests
+
 
 def to_usd(my_price):
      return f"${my_price:,.2f}" #> $12,000.71
@@ -37,16 +41,31 @@ latest_close = tsd[latest_day]["4. close"] #> 1000000
 
 
 high_prices = []
+low_prices = []
 
 for date in dates:
     high_price = tsd[date]["2. high"]
     high_prices.append(float(high_price))
+    low_price = tsd[date]["3. low"]
+    low_prices.append(float(low_price))
+
 
 recent_high = max(high_prices)
+recent_low = min(low_prices)
 #
 #
 #
+#csv_file_path = "data/prices.csv" # a relative filepath
 
+csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
+
+with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+    writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
+    writer.writeheader() # uses fieldnames set above
+    writer.writerow({"city": "New York", "name": "Yankees"})
+    writer.writerow({"city": "New York", "name": "Mets"})
+    writer.writerow({"city": "Boston", "name": "Red Sox"})
+    writer.writerow({"city": "New Haven", "name": "Ravens"})
 
 
 print("-------------------------")
@@ -58,10 +77,16 @@ print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print(f"RECENT HIGH: {to_usd(float(recent_high))}")
-print("RECENT LOW: $99,000.00")
+print(f"RECENT LOW: {to_usd(float(recent_low))}")
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
 print("-------------------------")
+print(f"WRITING DATA TO CSV: {csv_file_path} ")
+print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
+
+
+
+
